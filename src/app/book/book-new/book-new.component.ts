4dbb9458e7364ebad5a504ef938534790cd8ff2c
Dave <dave@webdave.de>
2024-04-19 13:09:31 +0200

    validator und form array

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,10 +1,13 @@
 import { Component, DestroyRef } from '@angular/core';
 import {
+  AbstractControl,
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
   NonNullableFormBuilder,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators
 } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
@@ -16,32 +19,50 @@ import { NgIf } from '@angular/common';
 import { MatInput, MatLabel } from '@angular/material/input';
 import { MatError, MatFormField } from '@angular/material/form-field';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatIconModule } from '@angular/material/icon';
 
 interface IBookForm {
   title: FormControl<string>;
   subtitle: FormControl<string>;
-  author: FormControl<string>;
+  // author: FormControl<string>;
+  authors: FormArray<FormControl<string>>;
   abstract: FormControl<string>;
   isbn: FormControl<string>;
   cover: FormControl<string>;
 }
+
+const authorValidator = (control: AbstractControl): ValidationErrors | null => {
+  return control.value === 'Memo' || control.value === 'Simon' ? null : { memo: 'Nur Memo oder Simon ist OK' };
+};
 
 @Component({
   selector: 'ws-book-new',
   styleUrls: ['./book-new.component.scss'],
   templateUrl: './book-new.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormField, MatInput, MatError, MatButton, RouterLink, MatLabel]
+  imports: [ReactiveFormsModule, MatFormField, MatInput, MatError, MatButton, RouterLink, MatLabel, MatIconModule]
 })
 export class BookNewComponent {
   protected form: FormGroup<IBookForm> = this.formBuilder.group({
     title: ['', [Validators.required]],
     subtitle: [''],
-    author: ['', [Validators.required]],
+    authors: this.formBuilder.array([] as string[]),
     abstract: [''],
-    isbn: ['', [Validators.required, Validators.minLength(3)]],
+    isbn: ['', [Validators.required, Validators.minLength(3), authorValidator]],
     cover: ['']
   });
+
+  get authors(): FormArray<FormControl<string>> {
+    return this.form.controls['authors'];
+  }
+
+  addAuthor() {
+    this.authors.push(new FormControl('', { nonNullable: true, validators: [authorValidator] }));
+  }
+
+  deleteAuthor(i: number) {
+    this.authors.removeAt(i);
+  }
 
   constructor(
     private readonly formBuilder: NonNullableFormBuilder,
